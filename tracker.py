d608600fc67c5fd04fadfd19f7d0daa7d7ef5343
mertsahiiin <mertsahin159@gmail.com>
2026-05-29 17:01:41 +0300

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -7,6 +7,7 @@ CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 message = "Amazon takip botu test mesajı ✅"
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-requests.post(url, data={"chat_id": CHAT_ID, "text": message})
+response = requests.post(url, data={"chat_id": CHAT_ID, "text": message})
 
-print("Telegram test mesajı gönderildi.")
+print("Status:", response.status_code)
+print("Response:", response.text)
