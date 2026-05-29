28f92b7220ca96b9fbe0f5baef5d37982172d524
mertsahiiin <mertsahin159@gmail.com>
2026-05-29 16:47:54 +0300

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,1 +1,12 @@
-print("Amazon takip botu çalıştı")
+import os
+import requests
+
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+
+message = "Amazon takip botu test mesajı ✅"
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+requests.post(url, data={"chat_id": CHAT_ID, "text": message})
+
+print("Telegram test mesajı gönderildi.")
