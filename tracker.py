2325bdf1f25e632ba3578bf9cb9d8f6f14984522
mertsahiiin <mertsahin159@gmail.com>
2026-05-29 17:42:23 +0300

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -6,16 +6,18 @@ TOKEN = os.environ["TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
 queries = [
-    "Amazon Depo Apple iPhone",
-    "Amazon Depo Apple Watch",
-    "Amazon Depo Apple AirPods",
-    "Amazon Depo Apple iPad",
-    "Amazon Depo Apple MacBook",
+    "Apple AirPods",
+    "Apple Watch",
+    "Apple iPad",
+    "Apple MacBook",
+    "Apple iPhone",
+    "Apple Pencil",
+    "Apple şarj adaptörü",
+    "Apple MagSafe",
 ]
 
-message = "🍎 Amazon Depo Apple takip linkleri:\n\n"
+message = "🍎 Amazon Türkiye Apple indirim takip linkleri:\n\n"
 
- for_query = []
 for q in queries:
     url = "https://www.amazon.com.tr/s?k=" + quote_plus(q)
     message += f"🔎 {q}\n{url}\n\n"
