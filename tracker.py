92bd53d919748bf99912a7ae7059f5d28d0b4580
mertsahiiin <mertsahin159@gmail.com>
2026-05-30 10:00:42 +0300

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -22,7 +22,9 @@ SEARCHES = [
 ]
 
 HEADERS = {
-    "User-Agent": "Mozilla/5.0"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+    "Accept-Language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
 }
 
 def send_telegram(text):
@@ -60,6 +62,8 @@ for query in SEARCHES:
 
     try:
         r = requests.get(url, headers=HEADERS, timeout=20)
+        print("Amazon status:", r.status_code)
+
         soup = BeautifulSoup(r.text, "html.parser")
         products = soup.select("[data-component-type='s-search-result']")[:5]
 
